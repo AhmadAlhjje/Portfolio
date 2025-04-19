adf03de16d6ea7ce5a866dbeb9f4b63e109eef93
obada63 <ahmadobadaal7jje@gmail.com>
2025-04-19 22:26:06 +0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const isProduction = mode === 'production'; // التحقق مما إذا كان الوضع هو الإنتاج
-
-  return {
-    plugins: [react()],
-    base: isProduction ? '/Portfolio/' : '/', // <--- استخدم '/' للتطوير و'/Portfolio/' للإنتاج
-  };
-});
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
